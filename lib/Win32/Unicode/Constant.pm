75a7fe718bf8eb50e4184c716ae06522332dc388
xaicron <xaicron@gmail.com>
2010-02-11 14:06:32 +0900

    compile speed up

--- lib/Win32/Unicode/Constant.pm
+++ lib/Win32/Unicode/Constant.pm
@@ -6,7 +6,7 @@ use Carp ();
 use Exporter 'import';
 
 our $VERISON = '0.11';
-our @EXPORT = grep { Win32::Unicode::Constant->can($_) } grep { !/import|BEGIN|EXPORT/ } keys %Win32::Unicode::Constant::;
+our @EXPORT = grep { !/import|BEGIN|EXPORT/ && Win32::Unicode::Constant->can($_) } keys %Win32::Unicode::Constant::;
 
 sub NULL     () { "\x00" }
 sub NULLP    () { [] }
