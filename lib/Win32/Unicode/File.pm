35a0cc21c327dbc8c27b30e64cad05619087eef6
xaicron <xaicron@gmail.com>
2010-03-18 16:55:41 +0900

    version 0.15

--- lib/Win32/Unicode/File.pm
+++ lib/Win32/Unicode/File.pm
@@ -21,7 +21,7 @@ our @EXPORT = qw/file_type file_size copyW moveW unlinkW touchW renameW/;
 our @EXPORT_OK = qw/filename_normalize slurp/;
 our %EXPORT_TAGS = ('all' => [@EXPORT, @EXPORT_OK]);
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 my %FILE_TYPE_ATTRIBUTES = (
     s => FILE_ATTRIBUTE_SYSTEM,
