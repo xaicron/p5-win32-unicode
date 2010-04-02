ea9eab0453fb0fbff7f2221592fb42aa2ee85e9f
xaicron <xaicron@gmail.com>
2010-04-02 16:28:36 +0900

    version 0.17

--- lib/Win32/Unicode/Error.pm
+++ lib/Win32/Unicode/Error.pm
@@ -7,7 +7,7 @@ use Win32::API ();
 use Carp ();
 use Exporter 'import';
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 use Win32::Unicode::Console;
 use Win32::Unicode::Constant;
--- lib/Win32/Unicode/Native.pm
+++ lib/Win32/Unicode/Native.pm
@@ -5,7 +5,7 @@ use warnings;
 use 5.008003;
 use Exporter 'import';
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 use Win32::Unicode::Console ':all';
 use Win32::Unicode::File ':all';
