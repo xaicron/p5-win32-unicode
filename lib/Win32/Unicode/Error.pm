7a393e6abc81aefe33cfe5b0e935cef4ffb40d70
xaicron <xaicron@gmail.com>
2010-06-29 00:14:47 +0900

    version 0.21

--- lib/Win32/Unicode/Error.pm
+++ lib/Win32/Unicode/Error.pm
@@ -6,7 +6,7 @@ use 5.008003;
 use Carp ();
 use Exporter 'import';
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 use Errno qw/:POSIX/;
 
--- lib/Win32/Unicode/XS.pm
+++ lib/Win32/Unicode/XS.pm
@@ -2,7 +2,7 @@ package Win32::Unicode::XS;
 use strict;
 use warnings;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 use XSLoader;
 XSLoader::load('Win32::Unicode', $VERSION);
