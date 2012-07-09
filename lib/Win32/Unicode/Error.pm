f35bdb424571137ae3f463daa7ca1d808cadffae
xaicron <xaicron@gmail.com>
2012-07-10 02:22:56 +0900

    fixed get last error timing

--- lib/Win32/Unicode/Error.pm
+++ lib/Win32/Unicode/Error.pm
@@ -25,13 +25,13 @@ sub errorW {
 
 my $ERROR_TABLE;
 sub _set_errno {
+    my $errno = $_[0] ? set_last_error($_[0]) : get_last_error();
     unless ($ERROR_TABLE) {
         require Errno;
         $ERROR_TABLE = {
             ERROR_FILE_EXISTS, => Errno::EEXIST(),
         };
     }
-    my $errno = $_[0] ? set_last_error($_[0]) : get_last_error();
     $! = $ERROR_TABLE->{$errno} || $errno;
     return;
 }
