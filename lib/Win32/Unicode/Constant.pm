c38abd27271ec94c5b0d04464726516e539bfae1
xaicron <xaicron@gmail.com>
2012-08-08 01:37:18 +0900

    version 0.38

--- lib/Win32/Unicode/Constant.pm
+++ lib/Win32/Unicode/Constant.pm
@@ -5,7 +5,7 @@ use warnings;
 use 5.008003;
 use Exporter 'import';
 
-our $VERSION = '0.37';
+our $VERSION = '0.38';
 our @EXPORT = grep { !/import|BEGIN|EXPORT/ && Win32::Unicode::Constant->can($_) } keys %Win32::Unicode::Constant::;
 
 use constant CYGWIN  => $^O eq 'cygwin';
--- lib/Win32/Unicode/File.pm
+++ lib/Win32/Unicode/File.pm
@@ -19,7 +19,7 @@ our @EXPORT = qw/file_type file_size copyW moveW unlinkW touchW renameW statW ut
 our @EXPORT_OK = qw/filename_normalize slurp/;
 our %EXPORT_TAGS = ('all' => [@EXPORT, @EXPORT_OK]);
 
-our $VERSION = '0.37';
+our $VERSION = '0.38';
 
 my %FILE_TYPE_ATTRIBUTES = (
     s => FILE_ATTRIBUTE_SYSTEM,
